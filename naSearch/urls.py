815a819fe59a2bf9d3f9b538e95204dc3a805299
Junny <junnyh196@gmail.com>
2021-07-15 15:32:51 +0900

    create bill_info interface

--- naSearch/urls.py
+++ naSearch/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # base_views.py
     path('', 
         base_views.index, name='index'),
-        
+    path('<int:bill_id>/', base_views.detail, name='detail'),
 ]
--- naSearch/views/base_views.py
+++ naSearch/views/base_views.py
@@ -10,3 +10,12 @@ def index(request):
     bill_list = [1, 2, 3, 4, 5, 6, 1, 1, 1, 1, 1, 1, 5, 6, 1, 1, 1, 1, 1, 1] # ?.objects.order_by('-create_date')
     context = {'bill_list': bill_list}
     return render(request, 'naSearch/bill_list.html', context)
+
+
+def detail(request, bill_id):
+    """
+    bill info
+    """
+    bill = 1
+    context = {'bill': bill}
+    return render(request, 'naSearch/bill_info.html', context)
