ed124b5b01ed1a1fc212b4d0601f11eca2258268
JuHoSon <zoohoson@gmail.com>
2021-08-13 23:44:32 +0900

    NA

--- naSearch/models.py
+++ naSearch/models.py
@@ -1,60 +1,12 @@
 from django.db import models
 import pandas as pd
-import numpy as np
-from gensim.models import FastText
 import pickle
-import math
+
 
 # Create your models here.
 
-data = pd.read_csv('naSearch/data/bill_21.csv')
-model_name = 'naSearch/data/hannanum_FastText.model'
-model = FastText.load(model_name)
-dic = dict()
-with open('naSearch/data/keyword_dic.pkl','rb') as f:
-    keyword_dic = pickle.load(f)
 
-class Answer(models.Model):
-    result = []
-
-    def Get_similarity(query):
-        similarity = dict()
-
-        for key in keyword_dic.keys():
-            if key in model.wv.vocab:
-                similarity[key] = model.wv.similarity(query,key)
-
-        result = sorted(similarity.items(),key=lambda x:x[1],reverse=True)[:20]
-        bill_list = dict()
-        max_v = 0
-        for key,value in result:
-            bill =keyword_dic[key]
-            for billno in bill:
-                if billno in bill_list.keys():
-                    tmp = bill_list[billno]
-                    tmp[1] += 1
-                    tmp[0] = value +tmp[0]
-                    if(max_v < tmp[1]) : max_v = tmp[1]
-                else:
-                    v = [value,1]
-                    bill_list[billno] = v
-        output = dict()
-
-        for key in bill_list.keys():
-            v = bill_list[key]
-            avg = v[0]/max_v
-            value = (v[1],avg)
-            output[key] = value
-        output = sorted(output.items(),key = lambda x:(-x[1][0],-x[1][1]))
-
-        res = []
-        print(output)
-
-        for key in output:
-            bill_no = key[0]
-            bill = data[data['billno'] == bill_no]['billname'].values
-            similarity = key[1][1]
-            list = [bill_no,bill,similarity]
-            res.append(list)
-
-        Answer.result = res
+class Model(models.Model):
+    bill_data = pd.read_csv('naSearch/data/bill_21.csv')
+    with open('naSearch/data/keyword_dic.pkl', 'rb') as f:
+        keyword_dic = pickle.load(f)
--- naSearch/views/base_views.py
+++ naSearch/views/base_views.py
@@ -1,39 +1,107 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from ..models import Answer
+from gensim.models import FastText
+from ..models import Model
+
+
+model_name = 'naSearch/data/hannanum_Fasttext.model'
+model = FastText.load(model_name)
+
 
 def main_page(request):
     """
     main Page
     """
-    return render(request, 'naSearch/main_page.html')
-
-
-def index(request):
-    """
-    bill List
-    """
     query_search = request.GET.get('query', '')
-    
     if query_search:
-        print(query_search)
-        Answer.Get_similarity(query_search)
-
-        bill_list = Answer()
-
-        context = {'bill_list' : bill_list}
-        return render(request, 'naSearch/bill_list.html', context)
-    else :
-        return render(request,'naSearch/bill_list.html',{'bill_list': None})
+        print('main', query_search)
+        return render(request, 'naSearch/bill_list.html', query_search)
+    else:
+        return render(request, 'naSearch/main_page.html')
 
 
-def detail(request, bill_id):
+def detail(request):
     """
     bill info
     """
     # bill = get_object_or_404(bill_model, pk=bill_id)  # 없는 bill_id요청시 404에러 하고싶은데, bill_model필요하다
-
+    bill_id = request.GET.get('bid', '')
     bill = 1
     context = {'bill': bill}
     return render(request, 'naSearch/bill_info.html', context)
+
+
+def Get_similarity(query):
+    data = Model.bill_data
+    # Keyword extracting through "Word Summarization by graph algorithm"  -> Must Check Paper, Accuracy !!
+    keyword_dic = Model.keyword_dic
+
+    # similarity : key = token of document, value = similarity between qudry and key
+    similarity = dict()
+    for key in keyword_dic.keys():
+        if key in model.wv.vocab:
+            similarity[key] = model.wv.similarity(query,key)
+
+    # Sorting token by high similarity with query
+    result = sorted(similarity.items(),key=lambda x: x[1], reverse=True)[:20]
+    
+    # bill_list : key = id of bill, value = [sum of simmilarity, count] of bill (same mean, document)
+    bill_list = dict()
+    max_v = 0
+    for key, value in result:
+        bill = keyword_dic[key]
+        for billno in bill:
+            if billno in bill_list.keys():
+                tmp = bill_list[billno]
+                tmp[1] += 1
+                tmp[0] = value + tmp[0]
+                if (max_v < tmp[1]): max_v = tmp[1]
+            else :
+                v = [value, 1]
+                bill_list[billno] = v
+
+    # output : key = id of bill, value = (count, average of similarity) of bill
+    output = dict()
+    for key in bill_list.keys():
+        v = bill_list[key]
+        avg = v[0] / max_v  # max_v 는 모든 의안 통틀어서 같은 값?
+        value = (v[1], avg)
+        output[key] = value
+
+    # Sorting bill by high (count, average of similarity) of bill with query
+    output = sorted(output.items(), key=lambda x: (-x[1][0], -x[1][1]))
+
+    # res는 아래 코드의 list 참고
+    res = []
+    for idx, key in enumerate(output):
+        bill_no = key[0]
+        bill = data[data['bill_no'] == bill_no]
+        bill_name = bill['bill_name'].values
+        bill_agency = bill['proposers'].values
+        # print(bill_agency)
+        bill_comm = 'null'  # bill['committee'].values
+        bill_date = bill['propose_date'].values
+        bill_keyword = ['하이데브', '파이팅', 'BISlab짱']
+
+        similarity = round(key[1][1], 2)
+        lst = [str(idx+1) + '(' + str(similarity) + ')',
+        bill_no, bill_name, bill_agency, bill_comm, bill_date, bill_keyword]
+        res.append(lst)
+    return res
+
+
+def index(request):
+    """
+    bill 목록 출력
+    """
+    query_search = request.GET.get('query', '')
+    if query_search:
+        bill_list = Get_similarity(query_search)
+
+        page = request.GET.get('page', '1')
+        paginator = Paginator(bill_list, 10)
+        page_obj = paginator.get_page(page)
+        context = {'bill_list':page_obj, 'query': query_search, 'page': page}
+        return render(request, 'naSearch/bill_list.html', context)
+    else :
+        return render(request,'naSearch/bill_list.html',{'bill_list': None})
